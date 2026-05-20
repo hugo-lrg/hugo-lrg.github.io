e5a45fc338d803b95255186bb27c2cd1002d494c
unknown <regenex0311@gmail.com>
2026-05-20 18:15:41 +0200

    added copy-404

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { copyFileSync } from 'node:fs'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
+    {
+      name: 'copy-404',
+      closeBundle() {
+        copyFileSync('dist/index.html', 'dist/404.html')
+      }
+    }
   ],
   base: '/',
   resolve: {
