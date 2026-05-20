7e7243e5c04e1093010ba022c08d79aa6ef7ffbd
unknown <regenex0311@gmail.com>
2026-05-20 18:08:08 +0200

    changed config

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
-  base: '/hugo-lrg.github.io/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
